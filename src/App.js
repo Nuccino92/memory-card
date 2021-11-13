13a8104da5274614f4f28598eb87cd7e4e2ccf9e
Nuccino92 <anthnucci@hotmail.com>
2021-11-13 00:59:11 -0500

    added header/GameBoard and image to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import GameBoard from "./components/GameBoard";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img
+        className="background-image"
+        src="./images/pokemon-background.jpg"
+        alt="background"
+      ></img>
+      <Header />
+      <GameBoard />
+    </div>
+  );
 }
 
 export default App;
