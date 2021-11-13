6d85b3ed45049eadb43e3ac61ed11cfaef90c504
Nuccino92 <anthnucci@hotmail.com>
2021-11-13 01:00:35 -0500

    simple header with downloaded image

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-image-container">
+        <img src="./images/Remember-that-Pokemon.png" alt="game-title"></img>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
