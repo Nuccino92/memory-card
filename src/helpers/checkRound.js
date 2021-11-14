d1b008a830a806ca6a6b8b276d6464539a5fb07b
Nuccino92 <anthnucci@hotmail.com>
2021-11-14 01:14:04 -0500

    completed check round function

--- src/helpers/checkRound.js
+++ src/helpers/checkRound.js
@@ -2,4 +2,26 @@ export const checkRound = (round, score) => {
   if (round === 0 && score === 4) {
     return true;
   }
+  if (round === 1 && score === 10) {
+    return true;
+  }
+  if (round === 2 && score === 18) {
+    return true;
+  }
+  if (round === 3 && score === 28) {
+    return true;
+  }
+  if (round === 4 && score === 40) {
+    return true;
+  }
+  if (round === 5 && score === 54) {
+    return true;
+  }
+  if (round === 6 && score === 70) {
+    return true;
+  }
+  if (round === 7 && score === 88) {
+    return true;
+  }
+  return false;
 };
